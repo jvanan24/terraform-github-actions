66a2eed1af4ca061f10ada673495a3006e454dd6
jvanandruel <jerome.vanandruel@decathlon.com>
2020-03-11 17:11:25 +0100

    SSH config

--- src/main.sh
+++ src/main.sh
@@ -126,12 +126,6 @@ function main {
       echo "$GH_PRIVATE_SSH" > ~/.ssh/id_rsa
       chmod 600 ~/.ssh/id_rsa
       ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null
-      cat <<EOF > ~/.ssh/config
-      host github.com
-        hostname github.com
-        user git
-        identityfile ~/.ssh/id_rsa
-      EOF
       echo "SSH config done"
   fi
 
