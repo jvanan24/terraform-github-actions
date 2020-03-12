4a7d68118385335f7d88edf13f541618e756324e
jvanandruel <jerome.vanandruel@decathlon.com>
2020-03-12 09:59:57 +0100

    SSH config

--- src/terraform_init.sh
+++ src/terraform_init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-ssh git@github.com -i /github/home/.ssh/id_rsa
-whoami
-
 function terraformInit {
   # Gather the output of `terraform init`.
   echo "init: info: initializing Terraform configuration in ${tfWorkingDir}"
