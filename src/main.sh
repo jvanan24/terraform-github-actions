d19b4fb928a696ffa1724eb635065654a94e85f0
jvanandruel <jerome.vanandruel@decathlon.com>
2020-03-12 10:10:57 +0100

    SSH config

--- src/main.sh
+++ src/main.sh
@@ -123,15 +123,15 @@ function main {
   else
       echo "Configuring SSH"
       mkdir -p ~/.ssh
-      mkdir -p /root/.ssh
+      #mkdir -p /root/.ssh
       echo "$GH_PRIVATE_SSH" > ~/.ssh/id_rsa
-      echo "$GH_PRIVATE_SSH" > /root/.ssh/id_rsa
+      #echo "$GH_PRIVATE_SSH" > /root/.ssh/id_rsa
       chmod 600 ~/.ssh/id_rsa
-      chmod 600 /root/.ssh/id_rsa
+      #chmod 600 /root/.ssh/id_rsa
       ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null
-      ssh-keyscan github.com >> /root/.ssh/known_hosts 2>/dev/null
+      #ssh-keyscan github.com >> /root/.ssh/known_hosts 2>/dev/null
       cp /src/ssh_config ~/.ssh/config
-      cp /src/ssh_config /root/.ssh/config
+      #cp /src/ssh_config /root/.ssh/config
       echo "SSH config done"
   fi
 
